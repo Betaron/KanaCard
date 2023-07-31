63f5f845f5adc94bc3e8272c77255145bb96da65
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2023-07-31 17:53:24 +0300

    Update Android API to 34

--- app/src/main/java/com/betaron/kanacard/ui/main/MainScreen.kt
+++ app/src/main/java/com/betaron/kanacard/ui/main/MainScreen.kt
@@ -2,14 +2,13 @@ package com.betaron.kanacard.ui.main
 
 import android.view.SoundEffectConstants
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.with
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -27,8 +26,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.DynamicFeed
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
@@ -46,7 +45,6 @@ import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Alignment.Companion.TopEnd
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
@@ -74,13 +72,11 @@ import com.betaron.kanacard.ui.theme.notoSerifJpRegular
 import kotlinx.coroutines.launch
 
 @OptIn(
-    ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class,
-    ExperimentalAnimationApi::class
+    ExperimentalMaterial3Api::class
 )
 @Composable
 fun MainScreen(
-    viewModel: MainViewModel = hiltViewModel(),
-    insetsManager: InsetsManager
+    viewModel: MainViewModel = hiltViewModel(), insetsManager: InsetsManager
 ) {
     var contentHeight by remember { mutableStateOf(0.dp) }
     val scope = rememberCoroutineScope()
@@ -95,8 +91,7 @@ fun MainScreen(
     val state = viewModel.state.value
     val transcriptions = stringArrayResource(R.array.transcription)
     val alphabets = listOf(
-        stringResource(R.string.hira),
-        stringResource(R.string.kata)
+        stringResource(R.string.hira), stringResource(R.string.kata)
     )
 
     val empty = remember { listOf(0, 37, 39, 47, 48, 49, 51, 52, 53, 54) }
@@ -112,18 +107,15 @@ fun MainScreen(
         )
     }
 
-    val shake = animateDpAsState(
-        targetValue = if (state.isCardShake) 0.dp else 0.1.dp,
-        animationSpec = repeatable(
-            iterations = 8,
+    val shake = animateDpAsState(targetValue = if (state.isCardShake) 0.dp else 0.1.dp,
+        animationSpec = repeatable(iterations = 8,
             repeatMode = RepeatMode.Reverse,
             animation = keyframes {
                 durationMillis = 30
                 (-16).dp at 10
                 16.dp at 20
-            }
-        )
-    )
+            }),
+        label = "Shake")
 
     var flipState by remember {
         mutableStateOf(CardFace.Front)
@@ -131,8 +123,7 @@ fun MainScreen(
 
     insetsManager.setUiWindowInsets(viewModel)
 
-    BottomSheetScaffold(
-        scaffoldState = scaffoldState,
+    BottomSheetScaffold(scaffoldState = scaffoldState,
         sheetPeekHeight = 0.dp,
         sheetSwipeEnabled = false,
 
@@ -143,8 +134,7 @@ fun MainScreen(
                     .fillMaxWidth(),
             ) {
                 BottomSheetDefaults.DragHandle(
-                    modifier = Modifier
-                        .align(CenterHorizontally)
+                    modifier = Modifier.align(CenterHorizontally)
                 )
             }
         },
@@ -156,16 +146,13 @@ fun MainScreen(
                             .getTop(localDensity)
                             .toDp()
                     })
-                    .fillMaxWidth(),
-                contentAlignment = Center
+                    .fillMaxWidth(), contentAlignment = Center
             ) {
-                SegmentedButton(
-                    items = alphabets,
+                SegmentedButton(items = alphabets,
                     defaultSelectedItemIndex = state.alphabet,
                     onItemSelection = {
                         viewModel.onEvent(MainEvent.SwitchAlphabet(it))
-                    }
-                )
+                    })
             }
         },
         sheetContent = {
@@ -175,8 +162,7 @@ fun MainScreen(
                 )
             )
             LaunchedEffect(state.isScaffoldCollapsed) {
-                if (state.isScaffoldCollapsed)
-                    viewModel.onEvent(MainEvent.SaveSelected)
+                if (state.isScaffoldCollapsed) viewModel.onEvent(MainEvent.SaveSelected)
             }
 
             Box(
@@ -186,79 +172,71 @@ fun MainScreen(
                 contentAlignment = TopCenter
             ) {
                 Column(
-                    modifier = Modifier
-                        .verticalScroll(rememberScrollState())
+                    modifier = Modifier.verticalScroll(rememberScrollState())
                 ) {
                     TableHeader(
-                        modifier = Modifier
-                            .align(CenterHorizontally),
+                        modifier = Modifier.align(CenterHorizontally),
                         title = stringResource(R.string.monographs),
                         tableItemsIds = monographs - empty.toSet(),
                         viewModel = viewModel
                     )
 
                     SymbolsTable(
-                        modifier = Modifier
-                            .height(550.dp),
+                        modifier = Modifier.height(550.dp),
                         ids = monographs,
                         spaces = empty,
                         columns = 5,
                         viewModel = viewModel
                     )
 
-                    Divider(
+                    HorizontalDivider(
                         modifier = Modifier
                             .align(CenterHorizontally)
                             .padding(8.dp)
                     )
 
                     TableHeader(
-                        modifier = Modifier
-                            .align(CenterHorizontally),
+                        modifier = Modifier.align(CenterHorizontally),
                         title = stringResource(R.string.monographs_with_diacritics),
                         tableItemsIds = monographsWithDiacritics,
                         viewModel = viewModel
                     )
 
                     SymbolsTable(
-                        modifier = Modifier
-                            .height(250.dp),
+                        modifier = Modifier.height(250.dp),
                         ids = monographsWithDiacritics,
                         columns = 5,
                         viewModel = viewModel
                     )
 
-                    Divider(
+                    HorizontalDivider(
                         modifier = Modifier
                             .align(CenterHorizontally)
                             .padding(8.dp)
                     )
 
                     TableHeader(
-                        modifier = Modifier
-                            .align(CenterHorizontally),
+                        modifier = Modifier.align(CenterHorizontally),
                         title = stringResource(R.string.digraphs),
                         tableItemsIds = digraphs,
                         viewModel = viewModel
                     )
 
                     SymbolsTable(
-                        modifier = Modifier
-                            .height(350.dp),
+                        modifier = Modifier.height(350.dp),
                         ids = digraphs,
                         columns = 3,
                         viewModel = viewModel
                     )
 
-                    Divider(
+                    HorizontalDivider(
                         modifier = Modifier
                             .align(CenterHorizontally)
                             .padding(8.dp)
                     )
 
                     TableHeader(
-                        modifier = Modifier
-                            .align(CenterHorizontally),
+                        modifier = Modifier.align(CenterHorizontally),
                         title = stringResource(R.string.digraphs_with_diacritics),
                         tableItemsIds = digraphsWithDiacritics,
                         viewModel = viewModel
@@ -274,115 +252,87 @@ fun MainScreen(
                     )
                 }
             }
-        }
-    ) { paddingValues ->
+        }) { paddingValues ->
         Box(
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxWidth()
         ) {
-            Column(
-                modifier = Modifier
-                    .onGloballyPositioned { coordinates ->
-                        contentHeight = with(localDensity) {
-                            coordinates.size.height.toDp() -
-                                    systemBarsPaddings.calculateTopPadding()
-                        }
+            Column(modifier = Modifier.onGloballyPositioned { coordinates ->
+                    contentHeight = with(localDensity) {
+                        coordinates.size.height.toDp() - systemBarsPaddings.calculateTopPadding()
                     }
-            ) {
+                }) {
                 Box(
-                    modifier = Modifier
-                        .weight(1f)
-                )
-                {
+                    modifier = Modifier.weight(1f)
+                ) {
                     AnimatedContent(
-                        targetState = state.currentSymbolIndex,
-                        transitionSpec = {
-                            slideInHorizontally { it } with slideOutHorizontally { -it }
-                        }
+                        targetState = state.currentSymbolIndex, transitionSpec = {
+                            slideInHorizontally { it } togetherWith slideOutHorizontally { -it }
+                        }, label = "CardSlide"
                     ) { targetSymbolIndex ->
-                        FlipCard(
-                            modifier = Modifier
-                                .offset(x = shake.value)
-                                .fillMaxSize()
-                                .padding(24.dp),
-                            cardFace = flipState,
-                            onClick = {
-                                localView.playSoundEffect(SoundEffectConstants.CLICK)
-                                flipState = it.next
-                            },
-                            axis = RotationAxis.AxisY,
-                            front = {
-                                Box(contentAlignment = TopEnd)
-                                {
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxSize(),
-                                        contentAlignment = Center
-                                    ) {
-                                        AnimatedContent(
-                                            targetState = state.alphabet
-                                        )
-                                        { targetAlphabet ->
-                                            AutoSizeText(
-                                                text = state.alphabetSymbols[targetAlphabet][targetSymbolIndex],
-                                                style = LocalTextStyle.current.merge(
-                                                    TextStyle(
-                                                        platformStyle = PlatformTextStyle(
-                                                            includeFontPadding = false
-                                                        ),
-                                                        lineHeightStyle = LineHeightStyle(
-                                                            alignment = LineHeightStyle.Alignment.Center,
-                                                            trim = LineHeightStyle.Trim.Both
-                                                        ),
-                                                        letterSpacing = (-24).sp
-                                                    )
-                                                ),
-                                                fontFamily = notoSerifJpRegular
-                                            )
-                                        }
-                                    }
-                                    IconButton(
-                                        modifier = Modifier
-                                            .padding(24.dp),
-                                        onClick = {
-                                            localView.playSoundEffect(SoundEffectConstants.CLICK)
-                                            scope.launch {
-                                                scaffoldState.bottomSheetState.expand()
-                                            }
-                                            keyboardController?.hide()
-                                        }
-                                    ) {
-                                        Icon(
-                                            imageVector = Icons.Outlined.DynamicFeed,
-                                            contentDescription = "Expand",
+                        FlipCard(modifier = Modifier
+                            .offset(x = shake.value)
+                            .fillMaxSize()
+                            .padding(24.dp), cardFace = flipState, onClick = {
+                            localView.playSoundEffect(SoundEffectConstants.CLICK)
+                            flipState = it.next
+                        }, axis = RotationAxis.AxisY, front = {
+                            Box(contentAlignment = TopEnd) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(), contentAlignment = Center
+                                ) {
+                                    AnimatedContent(
+                                        targetState = state.alphabet, label = "SoftSwitch"
+                                    ) { targetAlphabet ->
+                                        AutoSizeText(
+                                            text = state.alphabetSymbols[targetAlphabet][targetSymbolIndex],
+                                            style = LocalTextStyle.current.merge(
+                                                TextStyle(
+                                                    platformStyle = PlatformTextStyle(
+                                                        includeFontPadding = false
+                                                    ), lineHeightStyle = LineHeightStyle(
+                                                        alignment = LineHeightStyle.Alignment.Center,
+                                                        trim = LineHeightStyle.Trim.Both
+                                                    ), letterSpacing = (-24).sp
+                                                )
+                                            ),
+                                            fontFamily = notoSerifJpRegular
                                         )
                                     }
                                 }
-                            },
-                            back = {
-                                Box(
-                                    modifier = Modifier
-                                        .fillMaxSize(),
-                                    contentAlignment = Center
-                                ) {
-                                    AutoSizeText(
-                                        text = "[" + transcriptions[targetSymbolIndex] + "]",
-                                        style = LocalTextStyle.current.merge(
-                                            TextStyle(
-                                                platformStyle = PlatformTextStyle(
-                                                    includeFontPadding = false
-                                                ),
-                                                lineHeightStyle = LineHeightStyle(
-                                                    alignment = LineHeightStyle.Alignment.Center,
-                                                    trim = LineHeightStyle.Trim.Both
-                                                )
-                                            )
-                                        )
+                                IconButton(modifier = Modifier.padding(24.dp), onClick = {
+                                    localView.playSoundEffect(SoundEffectConstants.CLICK)
+                                    scope.launch {
+                                        scaffoldState.bottomSheetState.expand()
+                                    }
+                                    keyboardController?.hide()
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Outlined.DynamicFeed,
+                                        contentDescription = "Expand",
                                     )
                                 }
                             }
-                        )
+                        }, back = {
+                            Box(
+                                modifier = Modifier.fillMaxSize(), contentAlignment = Center
+                            ) {
+                                AutoSizeText(
+                                    text = "[" + transcriptions[targetSymbolIndex] + "]",
+                                    style = LocalTextStyle.current.merge(
+                                        TextStyle(
+                                            platformStyle = PlatformTextStyle(
+                                                includeFontPadding = false
+                                            ), lineHeightStyle = LineHeightStyle(
+                                                alignment = LineHeightStyle.Alignment.Center,
+                                                trim = LineHeightStyle.Trim.Both
+                                            )
+                                        )
+                                    )
+                                )
+                            }
+                        })
                     }
                 }
 
@@ -392,8 +342,7 @@ fun MainScreen(
                     }
                 }
 
-                AnswerSection(
-                    viewModel = viewModel,
+                AnswerSection(viewModel = viewModel,
                     modifier = Modifier
                         .align(CenterHorizontally)
                         .padding(bottom = with(localDensity) {
@@ -411,8 +360,7 @@ fun MainScreen(
                         localView.playSoundEffect(SoundEffectConstants.CLICK)
                         viewModel.onEvent(MainEvent.CheckAnswer)
                         flipState = CardFace.Front
-                    }
-                )
+                    })
             }
         }
     }
