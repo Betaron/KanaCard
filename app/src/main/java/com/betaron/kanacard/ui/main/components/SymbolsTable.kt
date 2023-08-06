9b6cd69fe31955621b9664a86f2f64401e7718f6
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2023-08-06 13:29:24 +0300

    Fix multiple insertion into selected

--- app/src/main/java/com/betaron/kanacard/ui/main/components/SymbolsTable.kt
+++ app/src/main/java/com/betaron/kanacard/ui/main/components/SymbolsTable.kt
@@ -69,6 +69,7 @@ fun SymbolsTable(
                                         .plus(initialKey!!..key)
                                         .plus(key..initialKey!!)
                                         .minus(spaces.toSet())
+                                        .distinct()
                                         .toList()
                                 )
                             )
